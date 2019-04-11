c13d806373e9d5640766b488fbed4a2353763517
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-04-11 17:07:40 +0200

    Create my_strlwr.c

---
+++ my_strlwr.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2018
+** 
+** File description:
+** 
+*/
+
+#include <stdio.h>
+#include <string.h>
+#define bind_check printf
+
+void my_strlwr(char target_variable[]){
+    /*
+     * This function is used to change
+     * the character to lowercase and accept the formality.
+    */
+    int cpt_count;
+
+    for(cpt_count = 0; cpt_count < strlen(target_variable); cpt_count++)
+        if(target_variable[cpt_count] >= 65 && target_variable[cpt_count] <= 96){ // MAJ
+            bind_check("%c", target_variable[cpt_count] + 32);    
+        }
+
+        else if(target_variable[cpt_count] >= 97 && target_variable[cpt_count] <= 120){ // LOW
+            bind_check("%c", target_variable[cpt_count]);
+        }
+}
+
+int main()
+{
+    /*
+     * This function will simply call the function my_strlwr() and test the
+     * values.
+    */
+    my_strlwr("SALUT");
+}
