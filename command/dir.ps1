800302fd43e63597e7c540017614061275dd2ccc
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-05-29 19:16:38 +0200

    Update dir.ps1

--- command/dir.ps1
+++ command/dir.ps1
@@ -9,6 +9,7 @@ RTM build reference, because I'm stupid and forget...
 #>
 
 
+
 function check_directory(){
     $output_directory = (dir | sort -Property length -Descending | Select-Object -First 1 | ForEach-Object { $_.DirectoryName })
     echo $output_directory
@@ -17,4 +18,12 @@ function check_directory(){
 function list_directory($return_list){
     $directory_check = (dir $return_list | sort -Property Name -Descending | Select-Object -Property Name | ForEach-Object { $_.Name })
     echo $directory_check
+}
+
+function check_length(){
+    $total_size = 0
+    $check_size = (dir | ForEach-Object { $total_size =+ $_.length })
+    if($total_size -gt 100){
+        echo "$total_size"
+    }
 }
